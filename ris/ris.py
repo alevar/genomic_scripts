afd9bc2fba838ec844118342df61f0cfdd9c21e9
Sparrow <alevar@gmail.com>
2023-11-03 01:20:48 -0400

    donor/acceptor extraction

--- ris/ris.py
+++ ris/ris.py
@@ -14,10 +14,38 @@ def extract_donor_acceptor(fname):
     # return two dictionaries, one for donors and one for acceptors
     # each dictionary maps position to the gene name
 
-    donors = {}
-    acceptors = {}
+    # run gffread to standardize
+    cmd = ["gffread","-T","-F","-o","tmp.gtf",fname]
 
+    donors = {} # position to gene gene_id
+    acceptors = {} # position to gene gene_id
 
+    # iterate over lines of gtf to extract the donor acceptor sites
+    # donor is defined as the last base of the previous exon
+    # acceptor is defined as the first base of the current exon
+    with open("tmp.gtf","r") as inFP:
+        chain = []
+        gid = ""
+        for line in inFP:
+            if line[0]=="#":
+                continue
+            lcs = line.strip().split("\t")
+            
+            if lcs[2] == "transcript":
+                for i in range(1,len(chain)):
+                    donors[chain[i][0]] = gid
+                    acceptors[chain[i-1][0]] = gid
+                
+                # new transcript
+                chain = []
+                gid = lcs[8].split("gene_id ")[1].split(";")[0].strip("\"")
+
+            if lcs[2] == "exon":
+                chain.append((int(lcs[3]),int(lcs[4]),lcs[6])) # position: start,end,strand
+
+    
+    if os.path.exists("tmp.gtf"):
+        os.remove("tmp.gtf")
 
     return donors,acceptors
 
@@ -31,24 +59,14 @@ def process(name,m1,m2,args):
     return
 
 def run(args):
-    # iterate over r1 and r2 files in pairs (assumes the same order)
+    donors1,acceptors1 = extract_donor_acceptor(args.a1)
+    donors2,acceptors2 = extract_donor_acceptor(args.a2)
 
-    name = ""
-    r1_seq = ""
-    r2_seq = ""
-    for line1,line2 in zip(open(args.i1,"r"),open(args.i2,"r")):
-        if line1[0]==">" and line2[0]==">":
-            if not name=="":
-                # process the previous pair
-                process(name,r1_seq,r2_seq,args)
-            
-            assert line1[1:].strip()==line2[1:].strip(), "Sequence IDs do not match: "+line1[1:].strip()+" vs "+line2[1:].strip()
-            name = line1[1:].strip()
-            r1_seq = ""
-            r2_seq = ""
-        else:
-            r1_seq+=line1.strip()
-            r2_seq+=line2.strip()
+    # what if instead of finding integration sites, we simply do the following:
+    # 1. find all reads which map to both genomes
+    # 2. search for the junction
+
+    return
 
 
 def main(args):
@@ -65,6 +83,14 @@ def main(args):
                         required=True,
                         type=str,
                         help="File containing mapping of reads to genome #2.")
+    parser.add_argument('-a1',
+                        required=True,
+                        type=str,
+                        help="GTF file containing gene annotations for genome #1.")
+    parser.add_argument('-a2',
+                        required=True,
+                        type=str,
+                        help="GTF file containing gene annotations for genome #2.")
     parser.add_argument('-o',
                         required=True,
                         type=str,
